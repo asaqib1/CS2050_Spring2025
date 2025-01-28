eb4e86ecedf523b45baacb9823bf7524a7ffb3b8
asaqib1 <alizahsaqib06@gmail.com>
2025-01-27 22:35:47 -0700

    workspace
    Setting up workspace for cs2050

---
+++ eclipseWorkSpace/cs2050ClassWork/SaqibFirstJavaProgram.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SaqibFirstJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
