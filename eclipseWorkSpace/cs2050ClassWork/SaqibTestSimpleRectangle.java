4204550b4035ffae8fb899e2a9df2665a824259a
asaqib1 <alizahsaqib06@gmail.com>
2025-02-03 16:31:55 -0700

    Create SaqibTestSimpleRectangle.java
    Started SimpleRectangle class for GE01 3.1

---
+++ eclipseWorkSpace/cs2050ClassWork/SaqibTestSimpleRectangle.java
@@ -0,0 +1,15 @@
+
+public class SaqibTestSimpleRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}//end main
+
+}//end SaqibTestSimpleRectangle class
+
+class SimpleRectangle {
+	
+	
+	
+}//end SimpleRectangle class
