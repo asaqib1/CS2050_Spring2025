357829f17d3ec3fdc2c6df8d6a070d31441f54c9
asaqib1 <alizahsaqib06@gmail.com>
2025-02-03 16:42:33 -0700

    Update SaqibTestSimpleRectangle.java
    created constructors, methods, and attributes for SimpleRectangle class for GE01 3.1

--- eclipseWorkSpace/cs2050ClassWork/SaqibTestSimpleRectangle.java
+++ eclipseWorkSpace/cs2050ClassWork/SaqibTestSimpleRectangle.java
@@ -10,6 +10,29 @@ public class SaqibTestSimpleRectangle {
 
 class SimpleRectangle {
 	
+	private double length;
+	private double width;
 	
+	public SimpleRectangle() {}
 	
+	public SimpleRectangle(double newLength, double newWidth) {
+		this.length = newLength;
+		this.width = newWidth;
+	}
+	
+	public double getArea() {
+		return length * width;
+	}
+	
+	public double getPerimeter() {
+		return 2 * (length + width);
+	}
+	
+	public void setLength(double newLength) {
+		this.length = newLength;
+	}
+	
+	public void setWidth(double newWidth) {
+		this.width = newWidth;
+	}
 }//end SimpleRectangle class
