5726ac1eb3064f75d0ef9b6d3433abcc3c45271e
asaqib1 <alizahsaqib06@gmail.com>
2025-02-03 17:21:03 -0700

    added code to print out the area and perimeter for each rectangle using the methods

--- eclipseWorkSpace/cs2050ClassWork/SaqibTestSimpleRectangle.java
+++ eclipseWorkSpace/cs2050ClassWork/SaqibTestSimpleRectangle.java
@@ -8,7 +8,17 @@ public class SaqibTestSimpleRectangle {
 		SimpleRectangle rectangle2 = new SimpleRectangle(1, 1);
 		SimpleRectangle rectangle3 = new SimpleRectangle(3, 4);
 		
+		System.out.println("Rectangle 1 information: \narea - " 
+							+ rectangle1.getArea() + " \nperimeter - " 
+							+ rectangle1.getPerimeter());
 		
+		System.out.println("\nRectangle 2 information: \narea - " 
+				+ rectangle2.getArea() + " \nperimeter - " 
+				+ rectangle2.getPerimeter());
+		
+		System.out.println("\nRectangle 3 information: \narea - " 
+				+ rectangle3.getArea() + " \nperimeter - " 
+				+ rectangle3.getPerimeter());
 		
 	}//end main
 
