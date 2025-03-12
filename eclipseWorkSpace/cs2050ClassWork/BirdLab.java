019caab83e699fe7f1eb31c1979ee7f797618186
asaqib1 <alizahsaqib06@gmail.com>
2025-03-12 14:12:29 -0600

    In class lab exploring abstract and concrete classes, interfaces, and ArrayLists

---
+++ eclipseWorkSpace/cs2050ClassWork/BirdLab.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class BirdLab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Bird> birdList = new ArrayList<>();
+		
+	}//end main
+
+}//end BirdLab class
+
+abstract class Bird {
+	
+	String birdName;
+	String swimSpeed;
+	
+	public abstract void printInterestingFact(ArrayList<Bird> bird);
+	
+}//end abstract Bird class
+
+interface Swim {
+	
+	
+	
+}//end interface Swim
+
+class penguin extends Bird implements Swim{
+	
+	private int swim;
+	
+	@Override
+	public void printInterestingFact(ArrayList<Bird> bird) {
+		for(Bird birds : bird) {
+			System.out.print("I can't fly but I'm the fastest swimmer and deepest diver.");
+		}
+	}
+	
+}//end penguin class
