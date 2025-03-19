4fd095afb1ae222125c5b5cc0aa78381f8ebf6a5
asaqib1 <alizahsaqib06@gmail.com>
2025-03-18 21:50:32 -0600

    test workspace to explore different code. For this I explored 2D arrays and what they look like in memory.

---
+++ eclipseWorkSpace/cs2050ClassWork/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//2D array in memory
+		int[][] arrayOfArrays = new int[2][3];
+
+	}
+
+}
