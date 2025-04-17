c5f166a9d32e6766c44ad821d04e3313bd0e41a2
asaqib1 <alizahsaqib06@gmail.com>
2025-04-16 20:50:59 -0600

    created vending machine iteration 2 project and added header comment.

---
+++ eclipseWorkSpace/cs2050ClassWork/SaqibAlizahVendingMachineIteration2.java
@@ -0,0 +1,33 @@
+/*
+ * Car Vending Machine Program - Iteration 02
+ * ------------------------------------------
+ * This is an updated version of the Car Vending Machine project. 
+ * In this iteration, we moved from using a 2D array to a LinkedList 
+ * for storing car objects, making the system more flexible and dynamic. 
+ * We also added a HashMap to keep track of car locations, and used 
+ * an abstract Car class with two types of cars: Basic and Premium.
+ *
+ * New features include searching cars by manufacturer and type, 
+ * sorting the inventory by price or year, adding cars to a car wash queue, 
+ * and selling (removing) cars from the machine.
+ *
+ * Everything runs through a menu system, and cars are still loaded 
+ * from a text file at the start.
+ *
+ * Author: Alizah Saqib
+ * Course: CS 2050
+ * Instructor: Deborah Harding
+ * Date: 04/28/2025
+ */
+
+public class SaqibAlizahVendingMachineIteration2 {
+
+	static void main(String[] args) {
+
+	}//end main
+
+}//end public class
+
+abstract class CarI2 {
+	
+}
