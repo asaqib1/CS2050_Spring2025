ac80d2c9870d4177dae8840c78d59dcd8e4d6715
asaqib1 <alizahsaqib06@gmail.com>
2025-04-17 14:46:03 -0600

    added in code in main including switch branch. Moving on to testing next.

--- eclipseWorkSpace/cs2050ClassWork/SaqibAlizahVendingMachineIteration2.java
+++ eclipseWorkSpace/cs2050ClassWork/SaqibAlizahVendingMachineIteration2.java
@@ -27,12 +27,103 @@ import java.util.*;
 public class SaqibAlizahVendingMachineIteration2 {
 
 	static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the number of floors for the car vending machine: ");
+		while (!input.hasNextInt()) {
+			System.out.println("Enter a valid number.");
+			input.next();
+		}
+		int floor = input.nextInt();
+
+		System.out.printf("Enter the number of spaces for the car vending machine: %n");
+		while(!input.hasNextInt()) {
+			System.out.println("Enter a valid number.");
+			input.next();
+		}
+		int space = input.nextInt();
+
+		VendingMachineI2 newMachine = new VendingMachineI2(floor, space);
+		
+		int choice;
+		
+		do {
+			System.out.printf("%n=== Car Vending Machine Menu ===%n");
+			System.out.println("1. Load Car Data from File");
+			System.out.println("2. Display Vending Machine");
+			System.out.println("3. Retrieve a Car by Location (Floor & Space)");
+			System.out.println("4. Print Sorted Inventory (Price)");
+			System.out.println("5. Print Sorted Inventory (Year)");
+			System.out.println("6. Search for Cars (Manufacturer & Type)");
+			System.out.println("7. Add Car to Wash Queue");
+			System.out.println("8. Process Car Wash Queue");
+			System.out.println("9. Sell a Car");
+			System.out.println("10. Exit");
+
+			System.out.printf("%nEnter your choice: ");
+			choice = input.nextInt();
+
+			switch (choice) {
+			case 1:
+				System.out.print("Enter the file name: ");
+				String fileName = input.next();
+				readFromFile(fileName, newMachine);
+				break;
+			case 2:
+				newMachine.displayVendingMachine();
+				break;
+			case 3:
+				System.out.print("Enter floor: ");
+				int floorNumber = input.nextInt();
+				System.out.print("Enter space: ");
+				int spaceNumber = input.nextInt();
+				newMachine.retrieveCar(floorNumber, spaceNumber);
+				break;
+			case 4:
+				newMachine.displaySortedCars("Price");
+				break;
+			case 5:
+				newMachine.displaySortedCars("Year");
+				break;
+			case 6: 
+				System.out.println("Enter manufacturer: ");
+				String manufacturer = input.next();
+				System.out.println("Enter car type (Basic/Premium): ");
+				String type = input.next();
+				newMachine.searchForCars(manufacturer, type);
+				break;
+			case 7: 
+				System.out.print("Enter floor: ");
+				int floorNum = input.nextInt();
+				System.out.print("Enter space: ");
+				int spaceNum = input.nextInt();
+				newMachine.retrieveCar(floorNum, spaceNum);
+				break;
+			case 8: 
+				newMachine.processWashQueue();
+				break;
+			case 9: 
+				System.out.print("Enter floor of the car to sell: ");
+				int sellFloor = input.nextInt();
+				System.out.print("Enter space of the car to sell: ");
+				int sellSpace = input.nextInt();
+				newMachine.removeSoldCar(sellFloor, sellSpace);
+				break;
+			case 10:
+				System.out.print("Exiting program. Goodbye!");
+				break;
+
+			}
+
+		} while (choice != 6);
+
+		input.close();
 
 	}//end main
 	
 	public static void readFromFile(String filename, VendingMachineI2 newMachine) {
 
-		// Initialize fileScanner to null so it can be correctly checked and closed in the finally block
 		Scanner fileScanner = null;
 
 		try {
@@ -85,6 +176,7 @@ abstract class CarI2 {
 	
 	public CarI2(String type, int year, double price, String manufacturer, String model, int floor, int space) {
 		
+		this.type = type;
 		this.year = year;
 		this.price = price;
 		this.manufacturer = manufacturer;
@@ -159,10 +251,12 @@ class VendingMachineI2 {
 	private int totalFloors;
 	private int totalSpaces;
 	
-	public VendingMachineI2() {
+	public VendingMachineI2(int totalFloors, int totalSpaces) {
 		vendingMachine = new LinkedList<>();
 		washQueue = new LinkedList<>();
 		locationMap = new HashMap<>();
+		this.totalFloors = totalFloors;
+		this.totalSpaces = totalSpaces;
 	}
 	
 	public void addCar(CarI2 currentCar, int Carfloor, int Carspace) {
@@ -184,10 +278,12 @@ class VendingMachineI2 {
 	public void displaySortedCars(String sortBy) {
 		List<CarI2> cars = new ArrayList<>(vendingMachine);
 		if(sortBy.equalsIgnoreCase("Price")){
+			System.out.println("Sorted Inventory by Price: ");
 			cars.sort(Comparator.comparing(CarI2::getPrice));
 		}
 
 		else if (sortBy.equalsIgnoreCase("Year")) {
+			System.out.println("Sorted Inventory by Year: ");
 			cars.sort(Comparator.comparing(CarI2::getYear));
 		}
 		
